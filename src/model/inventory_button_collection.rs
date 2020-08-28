facd3fc84ff844154ae0c8823d1654cf68cbd6fd
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-08-28 13:19:48 -0400

    add a displeasing selection of emojis

--- src/model/inventory_button_collection.rs
+++ src/model/inventory_button_collection.rs
@@ -18,4 +18,15 @@ impl InventoryButtonCollection {
     pub fn free_user_buttons(&self) -> u8 {
         (self.all().len() - DefaultItemType::all().len()) as u8 - MAX_USER_BUTTONS
     }
+
+    pub fn allowed_emojis() -> Vec<String> {
+        vec![
+            "😢", "😭", "😡", "😠", "😕", "🙄", "🙁", "😣", "😖", "😧", "😫", "😩", "🤯", "😞",
+            "😮", "😲", "😐", "😑", "😶", "😣", "🤐", "😔", "😨", "😬", "🥺", "😰", "😱", "😳",
+            "🤪", "🥴", "😵", "🤬", "🤢", "🤮",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect()
+    }
 }
